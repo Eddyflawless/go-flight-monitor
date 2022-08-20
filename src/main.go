b882d4dc58b22aae4496fa184ec6d26f8fdb1a15
darkvadr <edjonorh@gmail.com>
2022-08-20 16:00:20 +0000

    refactored controller out into a separate controller.go file. Added an additional route for websocket connection

--- src/main.go
+++ src/main.go
@@ -3,53 +3,35 @@ package main
 import(
 	"fmt"
 	"net/http"
-	// "github.com/Eddyflawless/flight-monitor/flight"
+	"github.com/joho/godotenv"
+	config "github.com/Eddyflawless/flight-monitor/config"
+	controller "github.com/Eddyflawless/flight-monitor/controller"
 	"log"
 )
 
-func appHeaders( w http.ResponseWriter){
-	w.Header().Set("Content-Type", "application/json")
-}
-
-func flightHistory(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method == "GET" {
-
-		appHeaders(w)
-		fmt.Fprintf(w, "")
-	}
-
-}
-
-func flightApi(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method == "GET" {
-
-		appHeaders(w)
-		fmt.Fprintf(w, "")
-	}
-
-}
-
-func IndexPage( w http.ResponseWriter, r *http.Request) {
-
-	if r.Method == "GET" {
-		fmt.Fprintf(w, "index page2..")
-
-	}else{
-		w.WriteHeader(405)
-	
-		w.Write([]byte("Method not supported"))
-	}
-}
+var configVar *config.ConfigVar 
 
 func setupRoutes() {
 
-	http.HandleFunc("/", IndexPage)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", controller.IndexPage)
+	http.HandleFunc("/ws/flights", controller.FlightWs)
+	http.HandleFunc("/flights", controller.FlightHistory)
+
+	port := fmt.Sprintf(":%s", configVar.Port)
+	log.Fatal(http.ListenAndServe(port, nil))
 	
 }
 
 func main() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading environment variables: %s", err)
+	}
+
+	configVar = config.GetVariables()
+
 	setupRoutes() // setupRoutes
+
 }
